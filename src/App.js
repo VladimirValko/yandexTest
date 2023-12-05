247b8aa44b965a93223836ae2d0850b884ce3176
Vladimir Valko <vladimirvalko@yandex.ru>
2023-12-05 15:32:51 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+
+import { YandexLogin, YandexLogout } from 'react-yandex-login';
+import './App.css'
+ 
+const clientID = '294cef12f775494895b0bcb9aa51b9d8';
+
+export const  App = () =>{
+  const [userData, setUserData] = useState(null);
+
+  const loginSuccess = (userData) => {
+    console.log('User Data: ', userData);
+    setUserData(userData)
+  }
+ 
+  const logoutSuccess = () => {
+    setUserData(null);
+  }
+//@ts-ignore
+  const sendMessageToApp = (userDataToSend) => {
+    //@ts-ignore
+    if (window.ReactNativeWebView) {
+      //@ts-ignore
+      window.ReactNativeWebView.postMessage(JSON.stringify(userDataToSend));
+    }
+  };
+
+  useEffect(() => {
+    sendMessageToApp(userData)
+  }, [userData])
+
+  const sampleData = {
+    name: "Oleg",
+    message: "Gde maket, oleg?",
+    number: 420
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+       <div>
+      {!userData && 
+      <>
+      <button onClick={() => sendMessageToApp(sampleData)}>Send message to app</button>
+        <YandexLogin clientID={clientID} onSuccess={loginSuccess}>
+          <button>Yandex Login</button>
+        </YandexLogin>
+      </>
+      }
+      {userData &&
+        <div>
+          <YandexLogout onSuccess={logoutSuccess}>
+            <button>Yandex Logout</button>
+          </YandexLogout>
+        </div>
+      }
     </div>
-  );
+    </>
+  )
 }
-
-export default App;
