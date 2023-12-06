8b0ba452f6ab40480939700ae6e90a9f99e312cb
Vladimir Valko <vladimirvalko@yandex.ru>
2023-12-06 16:14:23 +0300

    Yandex docs 1

--- src/App.js
+++ src/App.js
@@ -1,62 +1,29 @@
-
-import { useEffect, useState } from 'react';
-
-import { YandexLogin, YandexLogout } from 'react-yandex-login';
+import React, { useCallback } from 'react';
+import YandexAuth from './YandexAuth';
 import './App.css'
  
 const clientID = '294cef12f775494895b0bcb9aa51b9d8';
 
 function  App (){
-  const [userData, setUserData] = useState(null);
+  const redirectUri = 'https://vladimirvalko.github.io/yandexTest/';
 
-  const loginSuccess = (userData) => {
-    console.log('User Data: ', userData);
-    setUserData(userData)
-  }
- 
-  const logoutSuccess = () => {
-    setUserData(null);
-  }
-//@ts-ignore
-  const sendMessageToApp = (userDataToSend) => {
-    //@ts-ignore
-    if (window.ReactNativeWebView) {
-      //@ts-ignore
-      window.ReactNativeWebView.postMessage(JSON.stringify(userDataToSend));
-    }
-  };
+  const handleAuthSuccess = useCallback((data) => {
+      console.log('Yandex auth successful:', data);
+  }, []);
 
-  useEffect(() => {
-    sendMessageToApp(userData)
-  }, [userData])
-
-  const sampleData = {
-    name: "Oleg",
-    message: "Gde maket, oleg?",
-    number: 420
-  }
+  const handleAuthError = useCallback((error) => {
+      console.error('Yandex auth failed:', error);
+  }, []);
 
   return (
-    <>
-       <div>
-      {!userData && 
-      <>
-      <button onClick={() => sendMessageToApp(sampleData)}>Send message to app</button>
-        <YandexLogin clientID={clientID} onSuccess={loginSuccess}>
-          <button>Yandex Login</button>
-        </YandexLogin>
-      </>
-      }
-      {userData &&
-        <div>
-          <YandexLogout onSuccess={logoutSuccess}>
-            <button>Yandex Logout</button>
-          </YandexLogout>
-        </div>
-      }
-    </div>
-    </>
-  )
-}
+      <YandexAuth
+          clientId={clientID}
+          redirectUri={redirectUri}
+          onAuthSuccess={handleAuthSuccess}
+          onAuthError={handleAuthError}
+      />
+  );
+};
+
 
 export default App
---
+++ src/YandexAuth.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+
+const YandexAuth = ({ clientId, redirectUri, onAuthSuccess, onAuthError }) => {
+    useEffect(() => {
+        window.YaAuthSuggest.init(
+            {
+                client_id: clientId,
+                response_type: 'token',
+                redirect_uri: redirectUri
+            },
+            'https://vladimirvalko.github.io/yandexTest/'
+        )
+        .then(({ handler }) => handler())
+        .then(data => {
+            console.log('Authentication success:', data);
+            onAuthSuccess(data);
+        })
+        .catch(error => {
+            console.error('Authentication error:', error);
+            onAuthError(error);
+        });
+    }, [clientId, redirectUri, onAuthSuccess, onAuthError]);
+
+    return (
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <p>Authenticating with Yandex...</p>
+        </div>
+    );
+};
+
+export default YandexAuth;
