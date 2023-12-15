0ace7cb0d0bf7f81d329c36cd30d835f8e79c22d
Vladimir Valko <vladimirvalko@yandex.ru>
2023-12-15 13:19:41 +0300

    Yandex docs 17

--- src/YandexAuth.js
+++ src/YandexAuth.js
@@ -10,36 +10,49 @@ const YandexAuth = ({ setUserData }) => {
   }
 
     useEffect(() => {
-        window.YaAuthSuggest.init({
+        window.YaAuthSuggest.init(
+         {
             client_id: '294cef12f775494895b0bcb9aa51b9d8',
             response_type: 'token',
             redirect_uri: 'https://vladimirvalko.github.io/yandexTest/'
          },
-         'https://vladimirvalko.github.io/yandexTest/', {
-            view: 'button',
-            parentId: 'container',
-            buttonView: 'main',
-            buttonTheme: 'light',
-            buttonSize: 'm',
-            buttonBorderRadius: 0
-         }
+         'https://vladimirvalko.github.io/yandexTest/'
       )
-      .then(function(result) {
-         sendMessage("lalalalal from webview number 1")
-         return result.handler()
+      .then(({
+         handler
+      }) => handler())
+      .then(data => console.log('Сообщение с токеном', data))
+      .catch(error => console.log('Обработка ошибки', error));
+      //   ({
+      //       client_id: '294cef12f775494895b0bcb9aa51b9d8',
+      //       response_type: 'token',
+      //       redirect_uri: 'https://vladimirvalko.github.io/yandexTest/'
+      //    },
+      //    'https://vladimirvalko.github.io/yandexTest/', {
+      //       view: 'button',
+      //       parentId: 'container',
+      //       buttonView: 'main',
+      //       buttonTheme: 'light',
+      //       buttonSize: 'm',
+      //       buttonBorderRadius: 0
+      //    }
+      // )
+      // .then(function(result) {
+      //    sendMessage("lalalalal from webview number 1")
+      //    return result.handler()
 
-      })
-      .then(function(data) {
-         setUserData(data)
-         sendMessage(data)
-         alert(`${JSON.stringify(data)}`)
-         sendMessage("lalalalal from webview number 2")
-      })
-      .catch(function(error) {
-         console.log('Что-то пошло не так: ', error);
-         alert('Что-то пошло не так: ', error);
-         document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
-      });
+      // })
+      // .then(function(data) {
+      //    setUserData(data)
+      //    sendMessage(data)
+      //    alert(`${JSON.stringify(data)}`)
+      //    sendMessage("lalalalal from webview number 2")
+      // })
+      // .catch(function(error) {
+      //    console.log('Что-то пошло не так: ', error);
+      //    alert('Что-то пошло не так: ', error);
+      //    document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
+      // });
     }, []);
 
     return (
